1f4c28b20b62be742dadca94ef34995492b4787a
nasia <nasiasam@gmail.com>
2024-06-02 12:30:40 +0300

    data in db

--- db.ts
+++ db.ts
@@ -1,4 +1,43 @@
+import { Author } from "./src/types/entities/Author";
 
-export default  {
-  authors: [{ id: 1, name: 'Charles Dickens' }]
-}
+export default {
+  authors: [{ id: 1, name: "Charles Dickens" }],
+  books: [
+    {
+      id: 1,
+      title: "Great Expectations",
+      releasedAt: new Date(), // You can specify a release date here
+      author: { id: 1 } as Author,
+    },
+    {
+      id: 2,
+      title: "To Kill a Mockingbird",
+      releasedAt: new Date(),
+      author: { id: 1 } as Author,
+    },
+    {
+      id: 3,
+      title: "Pride and Prejudice",
+      releasedAt: new Date(),
+      author: { id: 1 } as Author,
+    },
+    {
+      id: 4,
+      title: "1984",
+      releasedAt: new Date(),
+      author: { id: 1 } as Author,
+    },
+    {
+      id: 5,
+      title: "The Catcher in the Rye",
+      releasedAt: new Date(),
+      author: { id: 1 } as Author,
+    },
+    {
+      id: 6,
+      title: "To the Lighthouse",
+      releasedAt: new Date(),
+      author: { id: 1 } as Author,
+    },
+  ],
+};
